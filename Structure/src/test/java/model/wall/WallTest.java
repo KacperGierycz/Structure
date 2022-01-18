35631eef3532d2c9fcacf7bcf8f5a851fea0bd77
KacperGierycz <kacpergierycz@gmail.com>
2022-01-18 11:44:00 +0100

    Setup of unit tests

--- Structure/src/test/java/model/wall/WallTest.java
+++ Structure/src/test/java/model/wall/WallTest.java
@@ -2,13 +2,26 @@ package model.wall;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class WallTest {
+	
+	Block block1=new BlockImpl("green","wool");
+	Block block2=new BlockImpl("black","iron");
+	List<Block> listOfBlocks= new ArrayList<Block>();
+	
+//	Wall wall=new Wall(block1);
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void wallConstructorTest() {
+		listOfBlocks.add(block1);
+		listOfBlocks.add(block2);
+		Wall wall=new Wall(listOfBlocks);
+		
+		
 	}
 
 }
